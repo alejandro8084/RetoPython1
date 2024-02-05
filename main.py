b359d9e744230d0aa87578104204cfcd63998191
alejandro8084 <107017764+alejandro8084@users.noreply.github.com>
2024-02-05 14:11:31 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# variables
+# tipos de datos
+# int - float
+# Operadores relacionales ( ==, !=,  <, <=, > , >=)
+# Operadores lógicos (and, or, not)
+# print - input - int - float - str
+
+
+nombre = input('Ingresa tu nombre: ')
+apellidos  = input("Ingresa tus apellidos: ")
+telefono = input("Ingresa tu número de telefono: ")
+Email = input("Ingresa tu correo electrónico: ")
+print(f"Hola {nombre} {apellidos}, en breve recibirás un correo a: {Email}")
